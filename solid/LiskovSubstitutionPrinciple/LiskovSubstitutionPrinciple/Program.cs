c2de2162cf0c67f4d4203bb52fae5d1b5542d2d9
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-03-28 01:35:14 -0400

    Solid - Liskov Substitution Principle

---
+++ solid/LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LiskovSubstitutionPrinciple
+{
+  public class Rectangle
+  {
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+
+    public Rectangle()
+    {
+      
+    }
+
+    public Rectangle(int width, int height)
+    {
+      Width = width;
+      Height = height;
+    }
+
+    public override string ToString()
+    {
+      return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+    }
+  }
+
+  public class Square : Rectangle
+  {
+    public override int Width
+    {
+      set => base.Width = base.Height = value;
+    }
+
+    public override int Height
+    {
+      set => base.Width = base.Height = value;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Rectangle rc = new Rectangle(2, 3);
+      Console.WriteLine($"{rc} has area {Area(rc)}");
+
+      Square sq = new Square();
+      sq.Width = 4;
+      Console.WriteLine($"{sq} has area {Area(sq)}");
+
+      Rectangle rc1 = new Square();
+      rc1.Width = 5;
+      Console.WriteLine($"{rc1} hase area {Area(rc1)}");
+    }
+
+    public static int Area(Rectangle rc) => rc.Width * rc.Height;
+  }
+}
